4df0a93726398288a5dbd094561b6e4d3a019a3d
Kim-Hj0 <149381535+Kim-Hj0@users.noreply.github.com>
2023-12-26 16:55:51 +0900

    feat : 기본 배치.
    유니티 세팅하기.
    시작씬, 메인씬 추가.

---
+++ Assets/Scripts/StartScene.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StartScene : MonoBehaviour
+{
+
+
+    //public void OnStartGameButtonClicked()  //시작신에서 버튼 누르면 메인신으로 넘어가기.
+    //{
+    //    PhotonNetwork.CurrentRoom.IsOpen = false;
+    //    PhotonNetwork.CurrentRoom.IsVisible = false;
+
+    //    PhotonNetwork.LoadLevel("MainScene");
+    //}
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
