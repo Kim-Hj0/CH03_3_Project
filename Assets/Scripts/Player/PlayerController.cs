409ea41f0af51ae6f1f3fea440d7d4e3a2d88ebb
Kim-Hj0 <149381535+Kim-Hj0@users.noreply.github.com>
2023-12-27 16:15:08 +0900

    feat : 캐릭터 idle 애니메이션

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -2,26 +2,35 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
     public event Action<Vector2> OnMoveEvent;
     public event Action<Vector2> OnLookEvent;
     public event Action OnJumpEvent;    //점프
+    public event Action OnSlideEvent;   //슬라이드
 
     public void CallMoveEvent(Vector2 direction)
     {
         OnMoveEvent?.Invoke(direction);
     }
 
-    public void CallLookEvent(Vector2 direction) 
-    {  
-        OnLookEvent?.Invoke(direction); 
+    public void CallLookEvent(Vector2 direction)
+    {
+        OnLookEvent?.Invoke(direction);
     }
 
     public void CallJumpEvent()
     { 
         OnJumpEvent?.Invoke();
     }
+
+    public void CallSlideEvent()
+    {
+        OnSlideEvent?.Invoke();
+    }
+
+    
 
 }
